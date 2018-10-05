8923b448a0c5bf7e5ad325d3e96b20c61f189ca5
Andrei Yunkevich <andreyha.by@gmail.com>
2018-10-05 10:57:58 +0300

    small refactored DataForCity

--- backend/src/api/DataForCity.js
+++ backend/src/api/DataForCity.js
@@ -1,7 +1,6 @@
 import rp from 'request-promise';
 
-const Pollen_index_over_past_year = async (code) => {
-  const days = 360;
+const Pollen_index_over_past_year = (code, days = 360) => {
   return new Promise(resolve => {
     const headers = {
       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36',
@@ -19,7 +18,7 @@ const Pollen_index_over_past_year = async (code) => {
       }))
       .catch(err => resolve(err));
   });
-}
+};
 
 module.exports = {
   Pollen_index_over_past_year,
