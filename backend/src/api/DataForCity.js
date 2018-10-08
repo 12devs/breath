67ba866c5bac2e57931a8cff720a2bf75dc486d4
andreigerasimchuk <gerasimchuk.grodno@gmail.com>
2018-10-08 13:58:43 +0300

    refactoring

--- backend/src/api/DataForCity.js
+++ backend/src/api/DataForCity.js
@@ -1,6 +1,7 @@
 import rp from 'request-promise';
 import fetch from 'node-fetch';
 import config from 'config';
+import { getCurrentLocation } from './location';
 
 const openweathermap = config.get('openweathermap');
 const google_api_key = config.get('google.api_key');
@@ -130,33 +131,6 @@ const COData = zipCode => {
     .then(res => Promise.resolve({ COData: res.data }))
     .catch(err => Promise.reject(err));
 
-}
-
-const getCurrentLocation = zipCode => {
-
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${google_api_key}`;
-
-  return fetch(url, { method: 'GET' })
-    .then(res => res.json())
-    .then(res => {
-
-      if (res.status !== 'OK') return Promise.reject(new Error('zip code not found'));  //@TODO null ?
-      if (!res.results.length) return Promise.reject(new Error('zip code not found'));  //@TODO null ?
-
-      const currentResult = res.results[0];
-      const { location } = currentResult.geometry;
-
-      const currentLocation = {
-        lat: (location.lat).toFixed(4),
-        lng: (location.lng).toFixed(4),
-        name: currentResult.address_components.find(elem => {
-          return elem.types.indexOf('locality') > -1;
-        }).long_name,
-      };
-
-      return Promise.resolve(currentLocation);
-    })
-    .catch(err => Promise.reject(err));
 }
 
 const pollenIndex = (code) => {
